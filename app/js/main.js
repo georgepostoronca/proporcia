411c4673c7f9b20e531cc11a30a1966c7ec5caaa
georgepostoronca <george.postoornca.work@gmail.com>
2020-01-15 00:57:29 +0300

    block header (Desktop)

--- app/js/main.js
+++ app/js/main.js
@@ -3,17 +3,19 @@
 
 // Plugins
 //= include ../../node_modules/jquery/dist/jquery.min.js
+//= include ../../node_modules/slick-carousel/slick/slick.min.js
 //= include ../../node_modules/svg4everybody/dist/svg4everybody.min.js
+//= include ../../node_modules/jquery-validation/dist/jquery.validate.js
 //= include ./jquery.maskedinput.min.js
 
-// Include All JS 
+// Include All JS
 //= include ../block/**/*.js
 
 // SVG
 svg4everybody();
 
 // Phone Mask
-$('#phone').mask("+9 (999) 999 99 99", {
+$('#phone').mask("+7 999 999 99 99", {
 	autoclear: true
 });
 
