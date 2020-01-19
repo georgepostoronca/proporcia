5c87abcc0d42e7948827edd446022f06be4cd5cc
georgepostoronca <george.postoornca.work@gmail.com>
2020-01-19 16:57:28 +0300

    dis favicon

--- dist/mail.php
+++ dist/mail.php
@@ -1,26 +1,30 @@
   
 <?php 
 
-$to  = "testmail@gmail.com";
+if($_POST['form-title']) {
+    $to  = "testmail@gmail.com";
 
-$subject = $_POST['form-title']; 
+    $subject = $_POST['form-title']; 
 
-$result = "";   
+    $result = "";   
 
-$index = 0;
-foreach($_POST as $value) {
-    $index++;
-    if($indexx == 1) {
-        continue;
-    } else {
-        $result .= "<tr><td>" . $value . "<td><tr>";
+    $index = 0;
+    foreach($_POST as $value) {
+        $index++;
+        if($indexx == 1) {
+            continue;
+        } else {
+            $result .= "<tr><td>" . $value . "<td><tr>";
+        }
     }
+    $message = "<table border='1' style='border-collapse: collapse;'><tbody>" . $result . "</tbody>" . "</table>";
+
+    $headers  = "Content-type: text/html; charset=UTF-8 \r\n"; 
+    $headers .= "From: От кого письмо <from@example.com>\r\n"; 
+    $headers .= "Reply-To: reply-to@example.com\r\n"; 
+
+    mail($to, $subject, $message, $headers); 
+    echo $_POST['form-title'];
 }
-$message = "<table border='1' style='border-collapse: collapse;'><tbody>" . $result . "</tbody>" . "</table>";
 
-$headers  = "Content-type: text/html; charset=UTF-8 \r\n"; 
-$headers .= "From: От кого письмо <from@example.com>\r\n"; 
-$headers .= "Reply-To: reply-to@example.com\r\n"; 
-
-mail($to, $subject, $message, $headers);
 ?>
